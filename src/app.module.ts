ff3e5edaf92ea808b2fb0b2f9a814c7d2bcf3e65
carlos <carloscarlangasxd@gmail.com>
2023-09-22 15:18:38 -0500

    antes de insertar por lote

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
@@ -16,7 +17,9 @@ import { CommonModule } from './common/common.module';
 
     PokemonModule,
 
-    CommonModule
+    CommonModule,
+
+    SeedModule
   ],
 })
 export class AppModule {}
